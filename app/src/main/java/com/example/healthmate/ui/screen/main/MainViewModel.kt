3d076dbd3a0c672f4591a85af45ae5d1ee1d2556
purboyndra <purboindra@gmail.com>
2025-03-29 13:36:26 +0700

    feat: MainViewModel

---
+++ app/src/main/java/com/example/healthmate/ui/screen/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.healthmate.ui.screen.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {}
