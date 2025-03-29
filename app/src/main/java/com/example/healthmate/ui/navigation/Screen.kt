1841d5810439c1734d79e734104194d5df1eccd2
purboyndra <purboindra@gmail.com>
2025-03-30 05:15:39 +0700

    feat: Screen route

---
+++ app/src/main/java/com/example/healthmate/ui/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.example.healthmate.ui.navigation
+
+sealed class Screen(val route: String) {
+    data object Splash : Screen("/splash")
+    data object Main : Screen("/main")
+    data object Home : Screen("/home")
+    data object Goal : Screen("/goal")
+    data object Exercise : Screen("/exercise")
+    data object Profile : Screen("/profile")
+}
