957d2b82a99aede5f70ab0e358af3cfe224abb63
purboyndra <purboindra@gmail.com>
2025-03-30 05:15:27 +0700

    feat: implement bottomnavbar MainScreen

--- app/src/main/java/com/example/healthmate/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/example/healthmate/ui/screen/main/MainScreen.kt
@@ -1,51 +1,69 @@
 package com.example.healthmate.ui.screen.main
 
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorManager
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.health.connect.client.HealthConnectClient
-import androidx.health.connect.client.PermissionController
-import androidx.health.connect.client.permission.HealthPermission
-import androidx.health.connect.client.records.DistanceRecord
-import androidx.health.connect.client.records.ExerciseSessionRecord
-import androidx.health.connect.client.records.HeartRateRecord
-import androidx.health.connect.client.records.SpeedRecord
-import androidx.health.connect.client.records.StepsRecord
-import androidx.health.connect.client.records.TotalCaloriesBurnedRecord
-import androidx.health.connect.client.records.WeightRecord
-import com.example.healthmate.data.HealthConnectManager
-import kotlinx.coroutines.launch
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
+import com.example.healthmate.ui.component.AppBottomNavigationBar
+import com.example.healthmate.ui.navigation.Screen
+import com.example.healthmate.ui.screen.goal.GoalScreen
+import com.example.healthmate.ui.screen.home.HomeScreen
 
 @Composable
-fun MainScreen() {
-    Scaffold { paddingValues ->
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .padding(paddingValues)
-        ) {
-            Text("Hello From Health Mate")
+fun MainScreen(
+    mainViewModel: MainViewModel = hiltViewModel(),
+    navHostController: NavHostController,
+) {
+    
+    val bottomNavController = rememberNavController()
+    val currentDestination =
+        bottomNavController.currentBackStackEntryAsState().value?.destination?.route
+    
+    Scaffold(
+        bottomBar = {
+            AppBottomNavigationBar(
+                selectedItem = currentDestination ?: Screen.Home.route,
+                onSelectedItem = { route ->
+                    if (route != currentDestination) {
+                        bottomNavController.navigate(route) {
+                            launchSingleTop = true
+                            restoreState = true
+                            popUpTo(Screen.Main.route) {
+                                saveState = true
+                            }
+                        }
+                    }
+                }
+            )
         }
+    ) { paddingValues ->
+        
+        val graph = bottomNavController.createGraph(
+            startDestination = Screen.Home.route,
+        ) {
+            composable(
+                route = Screen.Goal.route
+            ) {
+                GoalScreen()
+            }
+            composable(
+                route = Screen.Home.route
+            ) {
+                HomeScreen()
+            }
+        }
+        
+        NavHost(
+            navController = bottomNavController,
+            modifier = Modifier.padding(paddingValues),
+            graph = graph
+        )
     }
 }
