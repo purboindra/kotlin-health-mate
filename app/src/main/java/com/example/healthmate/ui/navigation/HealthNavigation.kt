25eded4907e9ca1211564c92e9805f8da832e167
purboyndra <purboindra@gmail.com>
2025-03-30 05:16:08 +0700

    feat: composable goal and home HealthNavigation

--- app/src/main/java/com/example/healthmate/ui/navigation/HealthNavigation.kt
+++ app/src/main/java/com/example/healthmate/ui/navigation/HealthNavigation.kt
@@ -4,18 +4,29 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.healthmate.ui.screen.goal.GoalScreen
+import com.example.healthmate.ui.screen.home.HomeScreen
 import com.example.healthmate.ui.screen.main.MainScreen
 
 @Composable
 fun HealthNavigation(
     navHostController: NavHostController
 ) {
-    
-    NavHost(navHostController, startDestination = "/main") {
+    NavHost(navHostController, startDestination = Screen.Main.route) {
         composable(
-            route = "/main"
+            route = Screen.Main.route
         ) {
-            MainScreen()
+            MainScreen(navHostController = navHostController)
+        }
+        composable(
+            route = Screen.Goal.route
+        ) {
+            GoalScreen()
+        }
+        composable(
+            route = Screen.Home.route
+        ) {
+            HomeScreen()
         }
     }
 }
