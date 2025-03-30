23eb16a95d2a939857bc91360c029b64231132be
purboyndra <purboindra@gmail.com>
2025-03-30 14:20:57 +0700

    optimize import AppBottomNavigationBar

--- app/src/main/java/com/example/healthmate/ui/component/AppBottomNavigationBar.kt
+++ app/src/main/java/com/example/healthmate/ui/component/AppBottomNavigationBar.kt
@@ -1,22 +1,11 @@
 package com.example.healthmate.ui.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.outlined.AddCircle
-import androidx.compose.material.icons.outlined.CheckCircle
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
