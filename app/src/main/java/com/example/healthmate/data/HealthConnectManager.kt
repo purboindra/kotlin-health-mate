450ea1d0cea0a3904dfeeb2c6471091a5739ee10
purboyndra <purboindra@gmail.com>
2025-03-30 20:53:11 +0700

    feat: write and read data health

--- app/src/main/java/com/example/healthmate/data/HealthConnectManager.kt
+++ app/src/main/java/com/example/healthmate/data/HealthConnectManager.kt
@@ -7,11 +7,9 @@ import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Build
 import android.util.Log
-import androidx.activity.ComponentActivity
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.HealthConnectClient.Companion.SDK_UNAVAILABLE
 import androidx.health.connect.client.PermissionController
@@ -23,6 +21,11 @@ import androidx.health.connect.client.records.SpeedRecord
 import androidx.health.connect.client.records.StepsRecord
 import androidx.health.connect.client.records.TotalCaloriesBurnedRecord
 import androidx.health.connect.client.records.WeightRecord
+import androidx.health.connect.client.records.metadata.Metadata
+import androidx.health.connect.client.units.Energy
+import androidx.health.connect.client.units.Mass
+import java.time.ZonedDateTime
+import kotlin.random.Random
 
 // The minimum android level that can use Health Connect
 const val MIN_SUPPORTED_SDK = Build.VERSION_CODES.O_MR1
@@ -112,4 +115,81 @@ class HealthConnectManager(private val context: Context) {
         healthConnectClient.permissionController.revokeAllPermissions()
     }
     
+    suspend fun writeWeightInput(weightInput: Double) {
+        val time = ZonedDateTime.now().withNano(0)
+        val weightRecord = WeightRecord(
+            time = time.toInstant(),
+            metadata = Metadata.manualEntry(),
+            weight = Mass.kilograms(weightInput),
+            zoneOffset = time.offset
+        )
+        val records = listOf(weightRecord)
+        try {
+            healthConnectClient.insertRecords(records)
+            Toast.makeText(
+                context,
+                "Successfully insert records!",
+                Toast.LENGTH_LONG
+            ).show()
+        } catch (e: Throwable) {
+            Toast.makeText(context, e.message.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+    
+    suspend fun writeExerciseSession(start: ZonedDateTime, end: ZonedDateTime) {
+        healthConnectClient.insertRecords(
+            listOf(
+                ExerciseSessionRecord(
+                    metadata = Metadata.manualEntry(),
+                    startTime = start.toInstant(),
+                    startZoneOffset = start.offset,
+                    endTime = end.toInstant(),
+                    endZoneOffset = end.offset,
+                    exerciseType = ExerciseSessionRecord.EXERCISE_TYPE_RUNNING,
+                    title = "My Run #${Random.nextInt(0, 60)}"
+                ),
+                StepsRecord(
+                    metadata = Metadata.manualEntry(),
+                    startTime = start.toInstant(),
+                    startZoneOffset = start.offset,
+                    endTime = end.toInstant(),
+                    endZoneOffset = end.offset,
+                    count = (1000 + 1000 * Random.nextInt(3)).toLong()
+                ),
+                TotalCaloriesBurnedRecord(
+                    metadata = Metadata.manualEntry(),
+                    startTime = start.toInstant(),
+                    startZoneOffset = start.offset,
+                    endTime = end.toInstant(),
+                    endZoneOffset = end.offset,
+                    energy = Energy.calories((140 + Random.nextInt(20)) * 0.01)
+                )
+            )
+        )
+    }
+    
+    private fun buildHeartRateSeries(
+        sessionStartTime: ZonedDateTime,
+        sessionEndTime: ZonedDateTime,
+    ): HeartRateRecord {
+        val samples = mutableListOf<HeartRateRecord.Sample>()
+        var time = sessionStartTime
+        while (time.isBefore(sessionEndTime)) {
+            samples.add(
+                HeartRateRecord.Sample(
+                    time = time.toInstant(),
+                    beatsPerMinute = (80 + Random.nextInt(80)).toLong()
+                )
+            )
+            time = time.plusSeconds(30)
+        }
+        return HeartRateRecord(
+            metadata = Metadata.manualEntry(),
+            startTime = sessionStartTime.toInstant(),
+            startZoneOffset = sessionStartTime.offset,
+            endTime = sessionEndTime.toInstant(),
+            endZoneOffset = sessionEndTime.offset,
+            samples = samples
+        )
+    }
 }
