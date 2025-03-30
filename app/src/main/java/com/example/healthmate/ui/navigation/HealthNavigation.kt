d2f9237b9318e0ac391e43bd654c61d5903265ee
purboyndra <purboindra@gmail.com>
2025-03-30 07:26:26 +0700

    fix: remove unused composable

--- app/src/main/java/com/example/healthmate/ui/navigation/HealthNavigation.kt
+++ app/src/main/java/com/example/healthmate/ui/navigation/HealthNavigation.kt
@@ -4,8 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.healthmate.ui.screen.goal.GoalScreen
-import com.example.healthmate.ui.screen.home.HomeScreen
 import com.example.healthmate.ui.screen.main.MainScreen
 
 @Composable
@@ -17,16 +15,6 @@ fun HealthNavigation(
             route = Screen.Main.route
         ) {
             MainScreen(navHostController = navHostController)
-        }
-        composable(
-            route = Screen.Goal.route
-        ) {
-            GoalScreen()
-        }
-        composable(
-            route = Screen.Home.route
-        ) {
-            HomeScreen()
         }
     }
 }
