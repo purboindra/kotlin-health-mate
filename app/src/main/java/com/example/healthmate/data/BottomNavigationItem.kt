f3a767b50f4e459259662d575348fa8c3a3cefc2
purboyndra <purboindra@gmail.com>
2025-03-30 12:51:20 +0700

    fix: bottomnavigation

--- app/src/main/java/com/example/healthmate/data/BottomNavigationItem.kt
+++ app/src/main/java/com/example/healthmate/data/BottomNavigationItem.kt
@@ -1,11 +1,12 @@
 package com.example.healthmate.data
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class BottomNavigationItem(
     val title: String,
-    val selectedItem: ImageVector,
-    val unSelectedItem: ImageVector,
+    val selectedItem: @Composable () -> Unit,
+    val unSelectedItem: @Composable () -> Unit,
     val hasNew: Boolean,
     val badgeCount: Int? = null,
     val route: String
--- app/src/main/java/com/example/healthmate/ui/component/AppBottomNavigationBar.kt
+++ app/src/main/java/com/example/healthmate/ui/component/AppBottomNavigationBar.kt
@@ -1,9 +1,10 @@
 package com.example.healthmate.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.AddCircle
 import androidx.compose.material.icons.outlined.CheckCircle
@@ -12,54 +13,129 @@ import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.healthmate.data.BottomNavigationItem
+import com.example.healthmate.ui.icons.MyIconPack
+import com.example.healthmate.ui.icons.myiconpack.Analyticsup
+import com.example.healthmate.ui.icons.myiconpack.Goal
+import com.example.healthmate.ui.icons.myiconpack.Home
+import com.example.healthmate.ui.icons.myiconpack.Profile
 import com.example.healthmate.ui.navigation.Screen
-
-private val items = listOf(
-    BottomNavigationItem(
-        title = "Home",
-        selectedItem = Icons.Filled.Home,
-        unSelectedItem = Icons.Outlined.Home,
-        hasNew = false,
-        route = Screen.Home.route
-    ),
-    BottomNavigationItem(
-        title = "Goal",
-        selectedItem = Icons.Filled.CheckCircle,
-        unSelectedItem = Icons.Outlined.CheckCircle,
-        hasNew = false,
-        route = Screen.Goal.route
-    ),
-    BottomNavigationItem(
-        title = "Exercise",
-        selectedItem = Icons.Filled.AddCircle,
-        unSelectedItem = Icons.Outlined.AddCircle,
-        hasNew = false,
-        route = Screen.Exercise.route
-    ),
-    BottomNavigationItem(
-        title = "Profile",
-        selectedItem = Icons.Filled.Person,
-        unSelectedItem = Icons.Outlined.Person,
-        hasNew = false,
-        route = Screen.Exercise.route
-    ),
-)
+import com.example.healthmate.ui.theme.GrayDark
+import com.example.healthmate.ui.theme.Green
 
 @Composable
 fun AppBottomNavigationBar(
     selectedItem: String,
     onSelectedItem: (String) -> Unit
 ) {
+    
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedItem = {
+                Icon(
+                    imageVector = MyIconPack.Home,
+                    contentDescription = "Home",
+                    tint = Green,
+                    modifier = Modifier.size(28.dp)
+                )
+            },
+            unSelectedItem = {
+                Icon(
+                    imageVector = MyIconPack.Home,
+                    contentDescription = "Home",
+                    tint = GrayDark,
+                    modifier = Modifier.size(28.dp)
+                )
+            },
+            hasNew = false,
+            route = Screen.Home.route
+        ),
+        BottomNavigationItem(
+            title = "Goal",
+            selectedItem = {
+                Icon(
+                    imageVector = MyIconPack.Goal,
+                    contentDescription = "Goal",
+                    tint = Green,
+                    modifier = Modifier.size(28.dp)
+                )
+            },
+            unSelectedItem = {
+                Icon(
+                    imageVector = MyIconPack.Goal,
+                    contentDescription = "Goal",
+                    tint = GrayDark,
+                    modifier = Modifier.size(28.dp)
+                )
+            },
+            hasNew = false,
+            route = Screen.Goal.route
+        ),
+        BottomNavigationItem(
+            title = "Exercise",
+            selectedItem = {
+                Icon(
+                    imageVector = MyIconPack.Analyticsup,
+                    contentDescription = "Exercise",
+                    tint = Green,
+                    modifier = Modifier.size(28.dp)
+                )
+            },
+            unSelectedItem = {
+                Icon(
+                    imageVector = MyIconPack.Analyticsup,
+                    contentDescription = "Exercise",
+                    tint = GrayDark,
+                    modifier = Modifier.size(28.dp)
+                )
+            },
+            hasNew = false,
+            route = Screen.Exercise.route
+        ),
+        BottomNavigationItem(
+            title = "Profile",
+            selectedItem = {
+                Icon(
+                    imageVector = MyIconPack.Profile,
+                    contentDescription = "Profile",
+                    tint = Green,
+                    modifier = Modifier.size(28.dp)
+                )
+            },
+            unSelectedItem = {
+                Icon(
+                    imageVector = MyIconPack.Profile,
+                    contentDescription = "Profile",
+                    tint = GrayDark,
+                    modifier = Modifier.size(28.dp)
+                )
+            },
+            hasNew = false,
+            route = Screen.Exercise.route
+        ),
+    )
+    
     NavigationBar(containerColor = Color.White) {
         items.forEachIndexed { _, bottomNavigationItem ->
             NavigationBarItem(
                 selected = selectedItem == bottomNavigationItem.route,
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Color.White,
+                    selectedTextColor = Green,
+                    unselectedTextColor = GrayDark,
+                    
+                    ),
                 onClick = { onSelectedItem(bottomNavigationItem.route) },
                 icon = {
                     BadgedBox(
@@ -73,15 +149,17 @@ fun AppBottomNavigationBar(
                             }
                         }
                     ) {
-                        Icon(
-                            imageVector = if (bottomNavigationItem.route == selectedItem) bottomNavigationItem.selectedItem else bottomNavigationItem.unSelectedItem,
-                            contentDescription = bottomNavigationItem.title
-                        )
+                        if (bottomNavigationItem.route == selectedItem) {
+                            bottomNavigationItem.selectedItem()
+                        } else {
+                            bottomNavigationItem.unSelectedItem()
+                        }
                     }
                 },
-                alwaysShowLabel = false,
                 label = {
-                    bottomNavigationItem.title
+                    Text(
+                        bottomNavigationItem.title,
+                        )
                 }
             )
             
