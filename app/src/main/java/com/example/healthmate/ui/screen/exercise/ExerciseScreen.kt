f9d128f95cd88e3a391c8f1604023a1fe2282880
purboyndra <purboindra@gmail.com>
2025-03-30 20:51:40 +0700

    feat: ExerciseScreen

---
+++ app/src/main/java/com/example/healthmate/ui/screen/exercise/ExerciseScreen.kt
@@ -0,0 +1,14 @@
+package com.example.healthmate.ui.screen.exercise
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun ExerciseScreen(
+    modifier: Modifier = Modifier,
+    navHostController: NavHostController
+) {
+    Text("Exercise Screen")
+}
