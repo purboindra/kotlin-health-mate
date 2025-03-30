e6dfd7ad0280364143209231317975e6c87bfc3d
purboyndra <purboindra@gmail.com>
2025-03-30 20:51:48 +0700

    feat: ProfileScreen

---
+++ app/src/main/java/com/example/healthmate/ui/screen/profile/ProfileScreen.kt
@@ -0,0 +1,15 @@
+package com.example.healthmate.ui.screen.profile
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun ProfileScreen(
+    modifier: Modifier = Modifier,
+    navHostController: NavHostController
+) {
+    Text("Profile Screen")
+}
