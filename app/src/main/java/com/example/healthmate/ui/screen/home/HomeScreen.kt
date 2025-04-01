b8070302fb0a1b674e164e5768e576fa9798d835
purboyndra <purboindra@gmail.com>
2025-04-01 11:55:25 +0700

    fix: optimize import HomeScreen

--- app/src/main/java/com/example/healthmate/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/healthmate/ui/screen/home/HomeScreen.kt
@@ -2,13 +2,9 @@ package com.example.healthmate.ui.screen.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
