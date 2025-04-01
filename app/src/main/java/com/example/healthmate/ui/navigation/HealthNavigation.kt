02ffaad687dfadf4c01f5a78756da67671a38dcf
purboyndra <purboindra@gmail.com>
2025-04-01 11:03:44 +0700

    feat: pass healthConnectManager HealthNavigation

--- app/src/main/java/com/example/healthmate/ui/navigation/HealthNavigation.kt
+++ app/src/main/java/com/example/healthmate/ui/navigation/HealthNavigation.kt
@@ -1,20 +1,27 @@
 package com.example.healthmate.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.healthmate.data.HealthConnectManager
 import com.example.healthmate.ui.screen.main.MainScreen
 
 @Composable
 fun HealthNavigation(
     navHostController: NavHostController
 ) {
+    val context = LocalContext.current
     NavHost(navHostController, startDestination = Screen.Main.route) {
+        val healthConnectManager = HealthConnectManager(context)
         composable(
             route = Screen.Main.route
         ) {
-            MainScreen(navHostController = navHostController)
+            MainScreen(
+                navHostController = navHostController,
+                healthConnectManager = healthConnectManager
+            )
         }
     }
 }
