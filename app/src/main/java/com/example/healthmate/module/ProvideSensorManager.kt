d4a6d1dc0179751670832dcb5c76cb5d77cbb004
purboyndra <purboindra@gmail.com>
2025-04-01 19:34:24 +0700

    feat: ProvideSensorManager

---
+++ app/src/main/java/com/example/healthmate/module/ProvideSensorManager.kt
@@ -0,0 +1,20 @@
+package com.example.healthmate.module
+
+import android.content.Context
+import com.example.healthmate.data.SensorManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ProvideSensorManager {
+    @Provides
+    @Singleton
+    fun provideSensorManager(@ApplicationContext context: Context): SensorManager {
+        return SensorManager(context)
+    }
+}
