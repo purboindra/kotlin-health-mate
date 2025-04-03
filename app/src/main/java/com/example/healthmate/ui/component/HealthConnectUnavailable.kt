d0682e19ab9367a2c90a310a2abe63f1274712cc
purboyndra <purboindra@gmail.com>
2025-04-03 16:11:13 +0700

    feat: HealthConnectUnavailable

---
+++ app/src/main/java/com/example/healthmate/ui/component/HealthConnectUnavailable.kt
@@ -0,0 +1,24 @@
+package com.example.healthmate.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HealthConnectUnavailable() {
+    Scaffold { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Health Connect Unavailable")
+        }
+    }
+}
