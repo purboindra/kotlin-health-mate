a3e3f958014f36c2e5ca9fff57fa925db5d39f7a
purboyndra <purboindra@gmail.com>
2025-04-03 16:21:31 +0700

    fix: iconModifier strategy params RowIconWithText

--- app/src/main/java/com/example/healthmate/ui/component/RowIconWithText.kt
+++ app/src/main/java/com/example/healthmate/ui/component/RowIconWithText.kt
@@ -2,25 +2,22 @@ package com.example.healthmate.ui.component
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.healthmate.util.HorizontalSpacer
 
 @Composable
 fun RowIconWithText(
     icon: ImageVector,
     contentDescription: String,
-    iconModifier: Modifier? = null,
+    iconModifier: Modifier = Modifier.size(24.dp),
     text: String,
     style: TextStyle = LocalTextStyle.current
 ) {
@@ -28,8 +25,9 @@ fun RowIconWithText(
         Icon(
             icon,
             contentDescription = contentDescription,
-            modifier = iconModifier ?: Modifier.size(24.dp)
+            modifier = iconModifier,
         )
+        5.HorizontalSpacer()
         Text(
             text,
             style = style,
