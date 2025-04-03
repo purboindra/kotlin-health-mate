8a875d473d97a0e273bd0cd46611aaa4d45902e2
purboyndra <purboindra@gmail.com>
2025-04-03 10:15:06 +0700

    update ExerciseScreen

--- app/src/main/java/com/example/healthmate/ui/screen/exercise/ExerciseScreen.kt
+++ app/src/main/java/com/example/healthmate/ui/screen/exercise/ExerciseScreen.kt
@@ -2,18 +2,22 @@ package com.example.healthmate.ui.screen.exercise
 
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -24,13 +28,17 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.health.connect.client.permission.HealthPermission.Companion.PERMISSION_READ_HEALTH_DATA_IN_BACKGROUND
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.healthmate.data.HealthConnectManager
+import com.example.healthmate.ui.component.ProgressCircle
+import com.example.healthmate.ui.component.RowIconWithText
 import com.example.healthmate.ui.theme.GrayDark
+import com.example.healthmate.ui.theme.MintGreen
 import com.example.healthmate.util.HorizontalSpacer
 import com.example.healthmate.util.VerticalSpacer
 
@@ -43,7 +51,7 @@ fun ExerciseScreen(
     modifier: Modifier = Modifier,
     navHostController: NavHostController,
     exerciseViewModel: ExerciseViewModel = hiltViewModel(),
-    healthConnectManager: HealthConnectManager
+    healthConnectManager: HealthConnectManager,
 ) {
     
     val coroutineScope = rememberCoroutineScope()
@@ -51,6 +59,15 @@ fun ExerciseScreen(
     val weightList by exerciseViewModel.weightList.collectAsState()
     var loading by remember { mutableStateOf(false) }
     val backgroundReadAvailable by exerciseViewModel.backgroundReadAvailable.collectAsState()
+    val historyReadAvailable by exerciseViewModel.historyReadAvailable.collectAsState()
+    
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+    
+    val outerSize = screenHeight * 0.25f
+    val innerSize = screenHeight * 0.20f
+    
     
     val permissionLauncher = rememberLauncherForActivityResult(
         healthConnectManager.requestPermissionsActivityContract()
@@ -64,57 +81,67 @@ fun ExerciseScreen(
         loading = false
     }
     
-    LazyColumn(modifier = Modifier.safeContentPadding()) {
+    LazyColumn(
+        modifier = Modifier
+            .safeContentPadding()
+            .padding(horizontal = 16.dp),
+    ) {
         item {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                OutlinedTextField(
-                    value = weight,
-                    onValueChange = {
-                        exerciseViewModel.onWeightChange(it)
-                    },
-                    placeholder = {
-                        Text("Weight")
-                    },
-                    singleLine = true,
-                    maxLines = 1,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    shape = RoundedCornerShape(10.dp),
-                    colors = TextFieldDefaults.colors(
-                        focusedTextColor = GrayDark,
-                        focusedContainerColor = MaterialTheme.colorScheme.surface,
-                        unfocusedContainerColor = MaterialTheme.colorScheme.surface,
-                        unfocusedTextColor = MaterialTheme.colorScheme.onSurface
+            
+            val heightProgressCircle = screenHeight * 0.25f
+            
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(32.dp),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                /// HEADER
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        Icons.Outlined.Info,
+                        contentDescription = "Info",
+                        tint = GrayDark,
+                        modifier = Modifier.size(24.dp)
                     )
-                )
-                8.HorizontalSpacer()
-                ElevatedButton(onClick = {
-                    exerciseViewModel.writeWeightInput()
-                }) {
-                    Text("Add")
+                    8.HorizontalSpacer()
+                    Box(
+                        modifier = Modifier
+                            .size(32.dp)
+                            .clip(CircleShape)
+                            .background(MintGreen)
+                    
+                    )
                 }
             }
+            
             10.VerticalSpacer()
-            if (loading) {
-                CircularProgressIndicator()
-            } else Column {
-                if (weightList.isEmpty()) Text("No data")
-                else weightList.forEachIndexed { index, weightRecord ->
-                    val time = exerciseViewModel.formatInstantToDate(
-                        weightRecord.time,
-                        pattern = "E, MMM dd yyyy HH:mm:ss a",
-                        timeZone = "Asia/Jakarta"
-                    )
-                    Text("${"%.1f".format(weightRecord.weight.inKilograms)} kilograms $time")
-                }
-                if (!backgroundReadAvailable) ElevatedButton(
-                    onClick = {
-                        permissionLauncher.launch(
-                            setOf(PERMISSION_READ_HEALTH_DATA_IN_BACKGROUND)
-                        )
-                    }
-                ) {
-                    Text("Request Background Read")
-                }
+            
+            ProgressCircle(
+                heightProgressCircle = heightProgressCircle,
+            )
+            5.VerticalSpacer()
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                RowIconWithText(
+                    icon = Icons.Outlined.FavoriteBorder,
+                    text = "Poin Kardio",
+                    contentDescription = "Poin Kardio",
+                    style = MaterialTheme.typography.titleSmall.copy(
+                        fontWeight = FontWeight.SemiBold
+                    ),
+                )
+                8.HorizontalSpacer()
+                RowIconWithText(
+                    icon = Icons.Outlined.FavoriteBorder,
+                    text = "Langkah",
+                    contentDescription = "Langkah",
+                    style = MaterialTheme.typography.titleSmall.copy(
+                        fontWeight = FontWeight.SemiBold
+                    ),
+                )
             }
         }
     }
