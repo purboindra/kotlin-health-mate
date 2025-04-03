a9ec9dd664636b73bb6c1604e82f3f35ea090fc1
purboyndra <purboindra@gmail.com>
2025-04-03 16:03:33 +0700

    feat: add fab MainScreen

--- app/src/main/java/com/example/healthmate/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/example/healthmate/ui/screen/main/MainScreen.kt
@@ -1,7 +1,12 @@
 package com.example.healthmate.ui.screen.main
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,6 +36,16 @@ fun MainScreen(
         bottomNavController.currentBackStackEntryAsState().value?.destination?.route
     
     Scaffold(
+        floatingActionButton = {
+            if (currentDestination == "/exercise")
+                SmallFloatingActionButton(
+                    onClick = { },
+                    containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                    contentColor = MaterialTheme.colorScheme.secondary
+                ) {
+                    Icon(Icons.Filled.Add, "Add")
+                }
+        },
         bottomBar = {
             AppBottomNavigationBar(
                 selectedItem = currentDestination ?: Screen.Home.route,
