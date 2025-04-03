99fe88f3af3f0ff0c81c67fdec72f8f9f6b4a951
purboyndra <purboindra@gmail.com>
2025-04-03 10:55:39 +0700

    feat: CardioInformationItem

---
+++ app/src/main/java/com/example/healthmate/ui/component/CardioInformationItem.kt
@@ -0,0 +1,40 @@
+package com.example.healthmate.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardioInformationItem(
+    value: String,
+    label: String,
+) {
+    Box(
+        modifier = Modifier.width(72.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                value,
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF0BA6D5),
+                )
+            )
+            Text(
+                label,
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
