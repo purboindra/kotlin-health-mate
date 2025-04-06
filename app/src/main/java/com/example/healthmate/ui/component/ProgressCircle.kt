0cdc90b8fcf8488f64961490e86d65e326033d68
purboyndra <purboindra@gmail.com>
2025-04-06 16:50:24 +0700

    fix: implement real data ProgressCircle.kt

--- app/src/main/java/com/example/healthmate/ui/component/ProgressCircle.kt
+++ app/src/main/java/com/example/healthmate/ui/component/ProgressCircle.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,13 +15,27 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.healthmate.ui.screen.exercise.ExerciseViewModel
 import com.example.healthmate.ui.theme.Green
 import com.example.healthmate.util.dpToFloat
 
 @Composable
 fun ProgressCircle(
-    heightProgressCircle: Dp
+    heightProgressCircle: Dp,
+    exerciseViewModel: ExerciseViewModel
 ) {
+    val stepsCount by exerciseViewModel.stepsCount.collectAsStateWithLifecycle()
+    val distanceMeters by exerciseViewModel.distanceMeters.collectAsStateWithLifecycle()
+    val calories by exerciseViewModel.calories.collectAsStateWithLifecycle()
+    
+    /// HARDCODE GOALS
+    val cardioGoals = 1000f
+    val stepsGoals = 5000f
+    
+    val cardioProgress = (distanceMeters / cardioGoals).coerceIn(0.001f, 1f)
+    val stepsProgress = (stepsCount / stepsGoals).coerceIn(0.001f, 1f)
+    
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -30,16 +45,19 @@ fun ProgressCircle(
         GoalCircle(
             topOffeset = (heightProgressCircle / 8f).dpToFloat(),
             progressColor = Color(0xFF9DD1D2),
+            progress = cardioProgress,
         )
         
         GoalCircle(
             size = 150.dp,
             topOffeset = (heightProgressCircle / 8f).dpToFloat(),
             progressColor = Color(0xFF0BA6D5),
+            progress = stepsProgress,
         )
+        
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
-                "0",
+                stepsCount.toString(),
                 style = MaterialTheme.typography.headlineMedium.copy(
                     fontWeight = FontWeight.Bold,
                     fontSize = 48.sp,
