e7865866e714c0c69ad91c254e4b2beaef8342e4
purboyndra <purboindra@gmail.com>
2025-04-06 18:24:13 +0700

    feat: ExerciseOptionViewModel.kt

---
+++ app/src/main/java/com/example/healthmate/ui/screen/exercise_option/ExerciseOptionViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.healthmate.ui.screen.exercise_option
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ExerciseOptionViewModel @Inject constructor() : ViewModel() {}
