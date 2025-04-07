96f6d44a898d09419b365d01a6a50259e7fcfead
purboyndra <purboindra@gmail.com>
2025-04-07 21:27:49 +0700

    fix: use LocationRequest.Builder MapManager.kt

--- app/src/main/java/com/example/healthmate/data/MapManager.kt
+++ app/src/main/java/com/example/healthmate/data/MapManager.kt
@@ -2,11 +2,11 @@ package com.example.healthmate.data
 
 import android.content.Context
 import android.os.Looper
-import android.util.Log
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import org.osmdroid.util.GeoPoint
 
 class MapManager(private val context: Context) {
@@ -14,20 +14,19 @@ class MapManager(private val context: Context) {
     
     private var locationCallback: LocationCallback? = null
     
-    fun startLocationTracking( onLocationUpdate: (GeoPoint) -> Unit) {
-        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    fun startLocationTracking(onLocationUpdate: (GeoPoint) -> Unit) {
+        val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(context)
         
-        val request = LocationRequest.create().apply {
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            interval = 2000  // every 2 seconds
-            fastestInterval = 1000
-        }
+        val request =
+            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 2000)
+                .build()
         
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(result: LocationResult) {
                 val loc = result.lastLocation ?: return
                 val geoPoint = GeoPoint(loc.latitude, loc.longitude)
-                Log.d("LocationTracker", "Live location: $geoPoint")
+//                Log.d("LocationTracker", "Live location: $geoPoint")
                 onLocationUpdate(geoPoint)
             }
         }
