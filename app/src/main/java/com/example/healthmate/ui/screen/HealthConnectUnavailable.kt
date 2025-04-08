40ce7535abe2062f9afe757a657f0e91bf5ff734
purboyndra <purboindra@gmail.com>
2025-04-09 05:36:40 +0700

    fix: backhandle HealthConnectUnavailable.kt

--- app/src/main/java/com/example/healthmate/ui/screen/HealthConnectUnavailable.kt
+++ app/src/main/java/com/example/healthmate/ui/screen/HealthConnectUnavailable.kt
@@ -1,8 +1,8 @@
 package com.example.healthmate.ui.screen
 
 import android.graphics.BitmapFactory
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,13 +15,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.healthmate.ui.navigation.Screen
 
 @Composable
-fun HealthConnectUnavailable() {
+fun HealthConnectUnavailable(navHostController: NavHostController) {
     
     val context = LocalContext.current
     
@@ -32,6 +33,12 @@ fun HealthConnectUnavailable() {
         remember { BitmapFactory.decodeStream(healthConnectInputStream) }
     val imageBitmap = remember { bitmap.asImageBitmap() }
     
+    BackHandler {
+        navHostController.navigate(Screen.Main.route) {
+            popUpTo(0) { inclusive = true }
+        }
+    }
+    
     Scaffold { paddingValues ->
         Box(
             modifier = Modifier
@@ -39,7 +46,7 @@ fun HealthConnectUnavailable() {
                 .padding(paddingValues),
             contentAlignment = Alignment.Center
         ) {
-            Column {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Image(
                     bitmap = imageBitmap,
                     contentDescription = "Health Connect Logo",
