d7399da30aca3b619452dd34767a438ecfa7041c
purboyndra <purboindra@gmail.com>
2025-04-10 20:21:07 +0700

    fix: implement UI MyActivityDaily.kt

--- app/src/main/java/com/example/healthmate/ui/component/MyActivityDaily.kt
+++ app/src/main/java/com/example/healthmate/ui/component/MyActivityDaily.kt
@@ -1,18 +1,45 @@
 package com.example.healthmate.ui.component
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.healthmate.ui.theme.GrayDark
+import com.example.healthmate.util.VerticalSpacer
 
 @Composable
 fun MyActivityDaily(modifier: Modifier = Modifier) {
-    Box(
+    
+    
+    Column(
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
     ) {
-        Text("Daily Activity")
+        DateMyActivityDaily(modifier = modifier)
+        8.VerticalSpacer()
+        ChartMyActivityDaily(modifier = modifier)
+        8.VerticalSpacer()
+        CardioStepInformation(modifier = modifier)
+        18.VerticalSpacer()
+        HorizontalDivider(modifier, 1.dp, GrayDark.copy(alpha = 0.6f))
+        11.VerticalSpacer()
+        Box(modifier = modifier.padding(horizontal = 10.dp)) {
+            TextButton(onClick = {}) {
+                Text(
+                    "Lihat sumber data",
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        color = Color.Blue
+                    )
+                )
+            }
+        }
     }
 }
