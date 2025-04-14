cb382696337087b05cda73a7abf55afc925a3ab6
purboyndra <purboindra@gmail.com>
2025-04-14 23:15:41 +0700

    fix: Only show WalkInformation if hasStepSensor

--- app/src/main/java/com/example/healthmate/ui/screen/walk/WalkScreen.kt
+++ app/src/main/java/com/example/healthmate/ui/screen/walk/WalkScreen.kt
@@ -8,20 +8,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.healthmate.data.SensorManager
 import com.example.healthmate.ui.component.HeaderWalk
 import com.example.healthmate.ui.component.OsmMapView
 import com.example.healthmate.ui.component.WalkInformation
+import com.example.healthmate.ui.screen.exercise.ExerciseViewModel
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun WalkScreen(
     navHostController: NavHostController,
-    walkViewModel: WalkViewModel = hiltViewModel<WalkViewModel>()
+    walkViewModel: WalkViewModel = hiltViewModel<WalkViewModel>(),
+    exerciseViewModel: ExerciseViewModel = hiltViewModel<ExerciseViewModel>()
 ) {
+    
+    val context = LocalContext.current
+    
+    val sensorManager = remember {
+        SensorManager(
+            context = context, onStepUpdate = {}
+        )
+    }
     
     
     Scaffold { paddingValues ->
@@ -39,10 +53,12 @@ fun WalkScreen(
                 modifier = Modifier
             )
             
-            WalkInformation(
+            if (sensorManager.hasStepSensor()) WalkInformation(
                 modifier = Modifier,
-                walkViewModel = walkViewModel
+                walkViewModel = walkViewModel,
+                exerciseViewModel = exerciseViewModel
             )
+            
         }
     }
     
