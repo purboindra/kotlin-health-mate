7ec4087a763c1ec42c48f8c2c450a97c909e4f66
purboyndra <purboindra@gmail.com>
2025-04-28 06:06:27 +0700

    feat: trial set weekly training plans MyActivityScreen.kt

--- app/src/main/java/com/example/healthmate/ui/screen/my_activity/MyActivityScreen.kt
+++ app/src/main/java/com/example/healthmate/ui/screen/my_activity/MyActivityScreen.kt
@@ -15,25 +15,42 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.healthmate.data.HealthConnectManager
 import com.example.healthmate.ui.component.MyActivityDaily
 import com.example.healthmate.ui.component.MyActivityMonthly
 import com.example.healthmate.ui.component.MyActivityWeekly
 import com.example.healthmate.ui.component.PrimaryTextTabs
 import com.example.healthmate.util.HorizontalSpacer
 import com.example.healthmate.util.VerticalSpacer
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyActivityScreen(navHostController: NavHostController) {
     
+    val context = LocalContext.current
+    
+    val coroutineScope = rememberCoroutineScope()
+    
+    val healthConnectManager = remember {
+        HealthConnectManager(context)
+    }
+    
     var tabIndex by remember { mutableIntStateOf(0) }
+    
+    LaunchedEffect(Unit) {
+        healthConnectManager.readStepsByTimeRange()
+    }
     
     Scaffold(
         topBar = {
@@ -61,7 +78,13 @@ fun MyActivityScreen(navHostController: NavHostController) {
                     Icon(
                         Icons.Default.Add,
                         contentDescription = "Add",
-                        modifier = Modifier.size(24.dp)
+                        modifier = Modifier
+                            .size(24.dp)
+                            .clickable(enabled = true, onClick = {
+                                coroutineScope.launch {
+                                    healthConnectManager.writeWeeklyPlanExercise()
+                                }
+                            })
                     )
                     12.HorizontalSpacer()
                     Icon(
