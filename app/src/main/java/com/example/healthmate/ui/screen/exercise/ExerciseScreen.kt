4920f615c5af90039fd337981a6ffdb7800102d6
purboyndra <purboindra@gmail.com>
2025-05-02 06:34:48 +0700

    feat: show loading setplans ExerciseScreen.kt

--- app/src/main/java/com/example/healthmate/ui/screen/exercise/ExerciseScreen.kt
+++ app/src/main/java/com/example/healthmate/ui/screen/exercise/ExerciseScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
@@ -19,6 +20,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -71,6 +73,8 @@ fun ExerciseScreen(
     navHostController: NavHostController,
     exerciseViewModel: ExerciseViewModel = hiltViewModel(),
     healthConnectManager: HealthConnectManager,
+    loadingSetPlans: Boolean,
+    
 ) {
     var loading by remember { mutableStateOf(false) }
     
@@ -150,8 +154,10 @@ fun ExerciseScreen(
     }
     
     LazyColumn {
+        
         item {
             val heightProgressCircle = screenHeight * 0.25f
+            
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -238,9 +244,17 @@ fun ExerciseScreen(
                 }
             )
             
-            
-            
             24.VerticalSpacer()
+        }
+    }
+    
+    
+    if (loadingSetPlans) {
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.3f)),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
         }
     }
 }
